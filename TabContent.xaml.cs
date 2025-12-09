d4ccdd34c3139a9bc8ea7f55d39f67fe150a31d0
Zoller-Mate <122789680+Zoller-Mate@users.noreply.github.com>
2025-12-09 14:25:24 +0100

    kezdetek

---
+++ TabContent.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Bongo
+{
+    /// <summary>
+    /// Interaction logic for TabContent.xaml
+    /// </summary>
+    public partial class TabContent : UserControl
+    {
+        public TabContent()
+        {
+            InitializeComponent();
+            if (System.ComponentModel.DesignerProperties.GetIsInDesignMode(this))
+                return;
+        }
+
+
+
+        public string Url
+        {
+            get { return (string)GetValue(UrlProperty); }
+            set { SetValue(UrlProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Url.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty UrlProperty =
+            DependencyProperty.Register("Url", typeof(string), typeof(TabContent), new PropertyMetadata("https://www.bing.com"));
+
+        private void Browser_Initialized(object sender, EventArgs e)
+        {
+            Browser.Address = Url;
+        }
+    }
+}
